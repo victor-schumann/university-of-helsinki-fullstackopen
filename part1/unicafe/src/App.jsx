4af40f7cdf4afa8aefeb575f7d12c01e2b0ebec6
Victor Schumann <schumann@tutamail.com>
2024-06-04 15:29:23 +0100

    feat (unicafe): implement unicafe step 1 (exercise 1.6)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,58 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header />
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Button name={'good'} onClick={() => setGood(good + 1)} text="Good" />
+        <Button name={'neutral'} onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+        <Button name={'bad'} onClick={() => setBad(bad + 1)} text="Bad" />
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
 export default App
+
+// Begin components 
+const Header = () => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.onClick}>{props.name}</button>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good:</td>
+            <td>{props.good}</td>
+          </tr>
+          <tr>
+            <td>neutral:</td>
+            <td>{props.neutral}</td>
+          </tr>
+          <tr>
+            <td>bad:</td>
+            <td>{props.bad}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+
+  )
+}
