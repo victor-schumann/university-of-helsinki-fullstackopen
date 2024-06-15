16f7504146e803b84088e7c19ac5f863ea89e8ed
Victor Schumann <schumann@tutamail.com>
2024-06-15 15:41:33 +0100

    feat: phonebook step 4 (exercise 2.9*)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,20 +2,24 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: 123123123
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -30,16 +34,24 @@ const App = () => {
       return
     }
 
-    else {
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
-    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
   }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with: <input value={filter} onChange={handleFilterChange} />
+      </div>
+      
+      <h2>Add a new</h2>
 
       <form onSubmit={addPerson}>
         <div>
@@ -48,15 +60,14 @@ const App = () => {
         <div>
           number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
-
         <div>
           <button type="submit">add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-        <p key={index}>{person.name} {person.number}</p>
+      {filteredPersons.map(person => (
+        <div key={person.id}>{person.name} {person.number}</div>
       ))}
     </div>
   )
